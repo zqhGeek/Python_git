0323e7c9c90a0dd2a1c25176d70c49f6671b8601
zqh <zqhkey@sina.com>
2018-04-20 15:04:06 +0800

    auto push at 2018-04-20 15:04:05.951910

--- automate_git.py
+++ automate_git.py
@@ -2,4 +2,8 @@
 
 import subprocess
 
+import datetime
+
 subprocess.call(["git", "add", "."])
+subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())]) # 加上当前系统的时间
+subprocess.call(["git", "push"])
