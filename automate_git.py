effcfed2f99926ddacfb7e18aa680047373b59f6
zqhkey <zqhkey@qq.com>
2018-04-24 10:05:16 +0800

    auto push at 再次修改2018-04-24 10:05:16.652370

--- automate_git.py
+++ automate_git.py
@@ -8,8 +8,8 @@ subprocess.call(["git", "add", "."])
 print('请输入提交备注:', end='')
 txt = input()
 subprocess.call(["git", "commit", "-m", "auto push at " + txt + str(datetime.datetime.now())])  # 加上当前系统的时间
-pull = "master"
-print('请输入拉取的分支(默认master):', end='')
-pull = input()
-subprocess.call(["git", "pull", "server", pull])
-subprocess.call(["git", "push", "server", "master"])
+pull_push = "master"
+print('请输入拉取/推送的分支(默认master):', end='')
+pull_push = input()
+subprocess.call(["git", "pull", "server", pull_push])
+subprocess.call(["git", "push", "server", pull_push])
