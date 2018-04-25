edfe368aacbcff160f93946aeccbf8abb5600658
zqhkey <zqhkey@qq.com>
2018-04-25 10:35:43 +0800

    auto push at 123 2018-04-25 10:35:42.949413

--- automate_git.py
+++ automate_git.py
@@ -1,22 +1,44 @@
 #!/usr/bin/env python
-
+import json
 import subprocess
 
 import datetime
 
 import os
 
+dictObj = {
+    'path': '',
+    'branch': ''
+}
+
+fr = open("git.txt", "r", encoding='utf-8')
 print('请输入仓库路径:', end='')
+json_str = json.loads(fr.read())
 path = input()
-os.chdir(path)
-print(os.getcwd())
+if len(path) == 0:
+    if len(json_str['path']) == 0:
+        path = os.getcwd()
+    else:
+        path = json_str['path']
+else:
+    os.chdir(path)
+
+print(path)
+json_str['path'] = path
 subprocess.call(["git", "add", "."])
 print('请输入提交备注:', end='')
 txt = input()
-subprocess.call(["git", "commit", "-m", "auto push at " + txt + " "+str(datetime.datetime.now())])  # 加上当前系统的时间
+subprocess.call(["git", "commit", "-m", "auto push at " + txt + " " + str(datetime.datetime.now())])  # 加上当前系统的时间
 print('请输入拉取/推送的分支(默认develop):', end='')
 pull_push = input()
 if len(pull_push) == 0:
-    pull_push = "develop"
-subprocess.call(["git", "pull", "server", pull_push])
-subprocess.call(["git", "push", "server", pull_push])
+    if len(json_str['branch']) == 0:
+        pull_push = "develop"
+    else:
+        pull_push = json_str['branch']
+json_str['branch'] = pull_push
+subprocess.call(["git", "pull", "origin", pull_push])
+subprocess.call(["git", "push", "origin", pull_push])
+fw = open("git.txt", "w", encoding='utf-8')
+fw.write(json.dumps(json_str))
+fw.close()
--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+
+# 打开一个文件
 import os
 
-print(os.getcwd() , end='')
+f = open("foo.txt", "r", encoding='utf-8')
+
+# num = f.write("Python 是一个非常好的语言。\n是的，的确非常好!!\n")
+# print(num)
+print(f.read())
+# 关闭打开的文件
+f.close()
