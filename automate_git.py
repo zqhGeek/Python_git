5943cde5b24ad1d9661a51fd63b3451b39562f18
zqhkey <zqhkey@qq.com>
2018-04-25 11:27:40 +0800

    auto push at 12 2018-04-25 11:27:40.032166

--- automate_git.py
+++ automate_git.py
@@ -31,15 +31,18 @@ print('请输入提交备注:', end='')
 txt = input()
 subprocess.call(["git", "commit", "-m", "auto push at " + txt + " " + str(datetime.datetime.now())])  # 加上当前系统的时间
 print('请输入拉取/推送的分支(默认origin/develop):', end='')
-pull_push = input()
-remote = pull_push.split('/')
-if len(pull_push) == 0:
+input_str = input()
+if len(input_str) == 0:
     if len(json_str['branch']) == 0 or len(json_str['remote']) == 0:
         pull_push = "develop"
         remote = "origin"
     else:
         pull_push = json_str['branch']
         remote = json_str['remote']
+else:
+    split_str = input_str.split('/')
+    remote = split_str[0]
+    pull_push = split_str[1]
 json_str['branch'] = pull_push
 json_str['remote'] = remote
 subprocess.call(["git", "pull", remote, pull_push])
